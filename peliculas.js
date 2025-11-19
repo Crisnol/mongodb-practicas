31d11fcb731e390a87abbda0b8ee1c96dfbe1cff
Crisnol <nolasco.vargas.cristian.46@cbtis118.edu.mx>
2025-11-19 18:04:55 +0000

    se agraga colecciones

---
+++ peliculas.js
@@ -0,0 +1,6 @@
+use("Peliculas")
+db.createCollection("Avengers");
+db.createCollection("MIB");
+db.createCollection("Intellestelar");
+db.createCollection("Iron-Man");
+db.createCollection("4 fantasiticos");
