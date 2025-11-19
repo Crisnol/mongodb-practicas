792c0a6cf20370a2e26c42225f73e15aec422b78
Crisnol <nolasco.vargas.cristian.46@cbtis118.edu.mx>
2025-11-19 19:21:30 +0000

    se agregan practicas 2

---
+++ db["4 fantasticos"].js
@@ -0,0 +1,20 @@
+db["4 fantasticos"].updateOne(
+
+{_id:ObjectId("691e10bbf8c30e4d187d234f")},
+{$set: {nombre:"Cuatro Fantásticos"}}
+)
+
+db["4 fantasticos"].updateOne(
+
+{_id:ObjectId("691e10bbf8c30e4d187d2357")},
+{$set: {nombre:"Comic: Los 4 Fantásticos Vol. 6"}}
+)
+
+db.Avengers.updateOne(
+
+{_id:ObjectId("691e10bbf8c30e4d187d2357")},
+{$set: {nombre:"Comic: Los 4 Fantásticos Vol. 6"}}
+)
+
+
+
---
+++ playPractica2.js
@@ -0,0 +1,368 @@
+use("Peliculas")
+db["4 fantasticos"].insertMany([
+    {
+        nombre: "Cuatro Fantásticos",
+        año: 2005,
+        genero: "Ciencia ficción",
+        director: "Tim Story",
+        reparto: ["Ioan Gruffudd", "Jessica Alba", "Chris Evans", "Michael Chiklis"]
+    },
+    {
+        nombre: "Los 4 Fantásticos y Silver Surfer",
+        año: 2007,
+        genero: "Ciencia ficción",
+        director: "Tim Story",
+        reparto: ["Ioan Gruffudd", "Jessica Alba", "Chris Evans", "Michael Chiklis", "Doug Jones"]
+    },
+    {
+        nombre: "Los 4 Fantásticos: primeros pasos",
+        año: 2025,
+        genero: "Ciencia ficción",
+        director: "Matt Shakman",
+        reparto: ["Vanessa Kirby", "Pedro Pascal", "Joseph Quinn", "Ebon Moss-Bachrach"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos",
+        año: 1961,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 2",
+        año: 1962,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 3",
+        año: 1963,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 4",
+        año: 1964,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 5",
+        año: 1965,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 6",
+        año: 1966,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+    {
+        nombre: "Comic: Los 4 Fantásticos Vol. 7",
+        año: 1967,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Reed Richards", "Sue Storm", "Johnny Storm", "Ben Grimm"]
+    },
+
+])
+
+db.Avengers.insertMany([
+    {
+        nombre: "Los Vengadores",
+        año: 2012,
+        genero: "Ciencia ficción",
+        director: "Joss Whedon",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson"]
+    },
+    {
+        nombre: "Avengers: Era de Ultrón",
+        año: 2015,
+        genero: "Ciencia ficción",
+        director: "Joss Whedon",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson"]
+    },
+    {
+        nombre: "Avengers: Infinity War",
+        año: 2018,
+        genero: "Ciencia ficción",
+        director: "Anthony y Joe Russo",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Josh Brolin"]
+    },
+    {
+        nombre: "Avengers: Endgame",
+        año: 2019,
+        genero: "Ciencia ficción",
+        director: "Anthony y Joe Russo",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Brie Larson"]
+    },
+    {
+        nombre: "Avengers: The Kang Dynasty",
+        año: 2026,
+        genero: "Ciencia ficción",
+        director: "Destin Daniel Cretton",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Jonathan Majors"]
+    },
+    {
+        nombre: "Avengers: Civil War",
+        año: 2016,
+        genero: "Ciencia ficción",
+        director: "Anthony y Joe Russo",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Sebastian Stan"]
+    },
+    {
+        nombre: "Comic: Los Vengadores Vol. 1",
+        año: 1963,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Sebastian Stan"]
+    },
+    {
+        nombre: "Comic: Los Vengadores Vol. 2",
+        año: 1964,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Sebastian Stan"]
+    },
+    {
+        nombre: "Comic: Los Vengadores Vol. 3",
+        año: 1965,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Sebastian Stan"]
+    },
+    {
+        nombre: "Comic: Los Vengadores Vol. 4",
+        año: 1966,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Chris Evans", "Mark Ruffalo", "Chris Hemsworth", "Scarlett Johansson", "Sebastian Stan"]
+    }
+
+])
+
+db.Intellestelar.insertMany([
+    {
+        nombre: "Interstellar",
+        año: 2014,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain", "Michael Caine"]
+    },
+    {
+        nombre: "Inception",
+        año: 2010,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Ellen Page", "Tom Hardy"]
+    },
+    {
+        nombre: "The Dark Knight",
+        año: 2008,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine"]
+    },
+    {
+        nombre: "Dunkirk",
+        año: 2017,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Fionn Whitehead", "Tom Hardy", "Kenneth Branagh", "Cillian Murphy"]
+    },
+    {
+        nombre: "Memento",
+        año: 2000,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Guy Pearce", "Carrie-Anne Moss", "Joe Pantoliano", "Mark Boone Junior"]
+    },
+    {
+        nombre: "Batman Begins",
+        año: 2005,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Christian Bale", "Michael Caine", "Liam Neeson", "Katie Holmes"]
+    },
+    {
+        nombre: "The Prestige",
+        año: 2006,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Hugh Jackman", "Christian Bale", "Scarlett Johansson", "Michael Caine"]
+    },
+    {
+        nombre: "Insomnia",
+        año: 2002,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Al Pacino", "Robin Williams", "Hilary Swank", "Maura Tierney"]
+    },
+    {
+        nombre: "Following",
+        año: 1998,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["Jeremy Theobald", "Alex Haw", "Lucy Russell", "Dickon Tolson"]
+    },
+    {
+        nombre: "Tenet",
+        año: 2020,
+        genero: "Ciencia ficción",
+        director: "Christopher Nolan",
+        reparto: ["John David Washington", "Robert Pattinson", "Elizabeth Debicki", "Kenneth Branagh"]
+    }
+])
+
+db["Iron-Man"].insertMany([
+    {
+        nombre: "Iron Man",
+        año: 2008,
+        genero: "Ciencia ficción",
+        director: "Jon Favreau",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Terrence Howard", "Jeff Bridges"]
+    },
+    {
+        nombre: "Iron Man 2",
+        año: 2010,
+        genero: "Ciencia ficción",
+        director: "Jon Favreau",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Iron Man 3",
+        año: 2013,
+        genero: "Ciencia ficción",
+        director: "Shane Black",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Guy Pearce"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 1",
+        año: 1968,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 2",
+        año: 1970,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 3",
+        año: 1972,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 4",
+        año: 1974,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 5",
+        año: 1976,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 6",
+        año: 1978,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    },
+    {
+        nombre: "Comic: Iron Man Vol. 7",
+        año: 1980,
+        genero: "Ciencia ficción",
+        director: "Stan Lee",
+        reparto: ["Robert Downey Jr.", "Gwyneth Paltrow", "Don Cheadle", "Mickey Rourke"]
+    }
+])
+
+db.MIB.insertMany([
+    {
+        nombre: "Hombres de Negro",
+        año: 1997,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Hombres de Negro II",
+        año: 2002,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Rosario Dawson", "Rip Torn"]
+    },
+    {
+        nombre: "Hombres de Negro III",
+        año: 2012,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Josh Brolin", "Jemaine Clement"]
+    },
+    {
+        nombre: "Hombres de Negro IV",
+        año: 2019,
+        genero: "Ciencia ficción",
+        director: "F. Gary Gray",
+        reparto: ["Chris Hemsworth", "Tessa Thompson", "Liam Neeson", "Emma Thompson"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 1",
+        año: 1990,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 2",
+        año: 1992,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 3",
+        año: 1994,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 4",
+        año: 1996,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 5",
+        año: 1998,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    },
+    {
+        nombre: "Comic: Hombres de Negro Vol. 6",
+        año: 2000,
+        genero: "Ciencia ficción",
+        director: "Barry Sonnenfeld",
+        reparto: ["Will Smith", "Tommy Lee Jones", "Linda Fiorentino", "Vincent D'Onofrio"]
+    }
+])
